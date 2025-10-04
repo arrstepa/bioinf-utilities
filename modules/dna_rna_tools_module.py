d43745a8d2ad46e239447bb1887630ce6f9ec265
arrstepa <arrstepa@gmail.com>
2025-10-04 11:21:41 +0300

    Add dockstrings

--- modules/dna_rna_tools_module.py
+++ modules/dna_rna_tools_module.py
@@ -1,20 +1,53 @@
 def is_dna(sequence):
+    """
+    Checks if sequence is DNA
+
+    Arguments:
+    sequence: str
+
+    Returns bool
+    """
     dna_nucleotides = set('ATGCatgc')
     unique_nucleotides = set(sequence)
     return unique_nucleotides <= dna_nucleotides
 
 
 def is_rna(sequence):
+    """
+    Checks if sequence is RNA
+
+    Arguments:
+    sequence: str
+
+    Returns bool
+    """
     rna_nucleotides = set('AUGCaugc')
     unique_nucleotides = set(sequence)
     return unique_nucleotides <= rna_nucleotides
 
 
 def is_nucleic_acid(sequence):
+    """
+    Checks if sequence is nucleic acid
+
+    Arguments:
+    sequence: str
+
+    Returns bool
+    """
     return is_dna(sequence) != is_rna(sequence)
 
 
 def transcribe(sequence):
+    """
+    Replaces T/t with U/u, returns mRNA for sense DNA chain
+
+    Arguments:
+    sequence: str
+
+    Returns str
+    Raises warning if sequence not DNA
+    """
     if is_dna(sequence):
         return sequence.replace('T', 'U').replace('t', 'u')
     else:
@@ -22,10 +55,26 @@ def transcribe(sequence):
 
 
 def reverse(sequence):
+    """
+    Returns backwards sequence
+
+    Arguments:
+    sequence: str
+
+    Returns str
+    """
     return sequence[::-1]
 
 
 def complement(sequence):
+    """
+    Returns the chain according to the complementary rule: A=T(U),G=C
+
+    Arguments:
+    sequence: str
+
+    Returns str
+    """
     if is_dna(sequence):
         complementary_rule = str.maketrans('ATGCatgc', 'TACGtacg')
         return sequence.translate(complementary_rule)
@@ -35,12 +84,37 @@ def complement(sequence):
 
 
 def reverse_complement(sequence):
+    """
+    Returns backwards and complementary sequence
+
+    Arguments:
+    sequence: str
+
+    Returns str
+    """
     return reverse(complement(sequence))
 
 
 def drop_not_na(list_of_seq):
+    """
+    Removes sequences that are not nucleic acids
+
+    Arguments:
+    list_of_seq: list
+
+    Returns list
+    """
     return [seq for seq in list_of_seq if is_nucleic_acid(seq)]
 
 
 def process_sequences(list_of_seq, func):
+    """
+    Applies required function to the list of sequences
+
+    Arguments:
+    list_of_seq: list
+    func: function
+
+    Returns list
+    """
     return [func(seq) for seq in list_of_seq]

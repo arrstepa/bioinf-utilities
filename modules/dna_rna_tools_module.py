4d81ec5724f6c6fb89ce6a54f5ee2d2d62b2136b
arrstepa <arrstepa@gmail.com>
2025-10-04 10:39:57 +0300

    Fix bugs

--- modules/dna_rna_tools_module.py
+++ modules/dna_rna_tools_module.py
@@ -1,56 +1,46 @@
-def is_nucleic_acid(list_of_seq):
+def is_dna(sequence):
     dna_nucleotides = set('ATGCatgc')
+    unique_nucleotides = set(sequence)
+    return unique_nucleotides <= dna_nucleotides
+
+
+def is_rna(sequence):
     rna_nucleotides = set('AUGCaugc')
-    list_of_results = []
-    for sequence in list_of_seq:
-        unique_nucleotides = set(sequence)
-        is_dna = (unique_nucleotides <= dna_nucleotides)
-        is_rna = (unique_nucleotides <= rna_nucleotides)
-        list_of_results.append(is_dna != is_rna)
-    return list_of_results
+    unique_nucleotides = set(sequence)
+    return unique_nucleotides <= rna_nucleotides
 
 
-def transcribe(list_of_seq):
-    list_of_results = []
-    dna_nucleotides = set('ATGCatgc')
-    for sequence in list_of_seq:
-        unique_nucleotides = set(sequence)
-        is_dna = (unique_nucleotides <= dna_nucleotides)
-        if is_dna:
-            transcribed = sequence.replace('T', 'U').replace('t', 'u')
-            list_of_results.append(transcribed)
-        else:
-            list_of_results.append('not DNA')
-    return list_of_results
+def is_nucleic_acid(sequence):
+    return is_dna(sequence) != is_rna(sequence)
 
 
-def reverse(list_of_seq):
-    return [seq[::-1] for seq in list_of_seq]
+def transcribe(sequence):
+    if is_dna(sequence):
+        return sequence.replace('T', 'U').replace('t', 'u')
+    else:
+        return 'not DNA'
 
 
-def complement(list_of_seq):
-    list_of_results = []
-    dna_nucleotides = set('ATGCatgc')
-    for sequence in list_of_seq:
-        unique_nucleotides = set(sequence)
-        is_dna = (unique_nucleotides <= dna_nucleotides)
-        if is_dna:
-            complementary_rule_dna = str.maketrans('ATGCatgc', 'TACGtacg')
-            list_of_results.append(sequence.translate(complementary_rule_dna))
-        else:
-            complementary_rule_rna = str.maketrans('AUGCaugc', 'UACGuacg')
-            list_of_results.append(sequence.translate(complementary_rule_rna))
-    return list_of_results
+def reverse(sequence):
+    return sequence[::-1]
 
 
-def reverse_complement(list_of_complements):
-    return [seq[::-1] for seq in list_of_complements]
+def complement(sequence):
+    if is_dna(sequence):
+        complementary_rule = str.maketrans('ATGCatgc', 'TACGtacg')
+        return sequence.translate(complementary_rule)
+    else:
+        complementary_rule = str.maketrans('AUGCaugc', 'UACGuacg')
+        return sequence.translate(complementary_rule)
+
+
+def reverse_complement(sequence):
+    return reverse(complement(sequence))
 
 
 def drop_not_na(list_of_seq):
-    existing_seq_only = [
-        seq
-        for seq, is_nucleic in zip(list_of_seq, is_nucleic_acid(list_of_seq))
-        if is_nucleic
-    ]
-    return existing_seq_only
+    return [seq for seq in list_of_seq if is_nucleic_acid(seq)]
+
+
+def process_sequences(list_of_seq, func):
+    return [func(seq) for seq in list_of_seq]

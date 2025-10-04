b0bcbc58b20c186c6acd101930d9e6c25a9a2643
arrstepa <arrstepa@gmail.com>
2025-10-04 11:21:51 +0300

    Add dockstrings

--- modules/filter_fastq_module.py
+++ modules/filter_fastq_module.py
@@ -1,25 +1,76 @@
 def gc_perc(seq):
+    """
+    Computes the percentage of G and C in the sequence
+
+    Arguments:
+    seq: str
+
+    Returns int
+    """
     return ((seq.count('G') + seq.count('C')) / len(seq)) * 100
 
 
 def gc_filter(seq, bounds=(0,100)):
+    """
+    Removes sequences containing G and C outside the specified range
+
+    Arguments:
+    seq: str
+    bounds: tuple
+
+    Returns bool
+    """
     return bounds[0] <= gc_perc(seq) <= bounds[1]
 
 
 def length_filter(seq, bounds=(0, 2**32)):
+    """
+    Removes sequences of length out of the specified range
+
+    Arguments:
+    seq: str
+    bounds: tuple
+
+    Returns bool
+    """
     return bounds[0] <= len(seq) <= bounds[1]
 
 
 def quality_decode(quality=0):
+    """
+    Decodes quality string according to ASCII
+
+    Arguments:
+    quality: int
+
+    Returns list
+    """
     return [ord(x) - 33 for x in quality]
 
 
 def quality_filter(quality, threshold=0):
+    """
+    Removes sequences of quality out of the specified range
+
+    Arguments:
+    quality: str
+    threshold: int
+
+    Returns bool
+    """
     mean_quality = sum(quality_decode(quality))/len(quality)
     return mean_quality >= threshold
 
 
 def bounds(x):
+    """
+    If a single boundary number is given, it turns it into an interval
+
+    Arguments:
+    x: int/tuple
+
+    Returns tuple
+    """
     if type(x) == int:
         boundaries = (0, x)
     elif type(x) == tuple:

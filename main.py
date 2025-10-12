882bcff808a5b0e531b4275a9a23fe8007ffa99a
arrstepa <arrstepa@gmail.com>
2025-10-12 20:47:52 +0300

    Fix main function for reading and writing fastq files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from modules.filter_fastq_module import *
 from modules.dna_rna_tools_module import *
+import os
 
 
 def run_dna_rna_tools(*args):
@@ -47,6 +48,7 @@ def filter_fastq(input_fastq, gc_bounds, length_bounds, quality_threshold):
 
     Returns dict
     """
+    output_fastq = os.path.dirname(input_fastq)
     seqs = file_to_dict(input_fastq)
     filtrated_sequences = dict()
     for name, (sequence, quality) in seqs.items():
@@ -54,5 +56,7 @@ def filter_fastq(input_fastq, gc_bounds, length_bounds, quality_threshold):
                 length_filter(sequence, bounds(length_bounds)) and
                 quality_filter(quality, quality_threshold)):
             filtrated_sequences[name]=(sequence,quality)
-    return filtrated_sequences
+    return filtrated_fastq_to_file(filtrated_sequences, output_fastq)
 
+
+filter_fastq("/Users/arrrstepa/bioinf2025_python/bioinf-utilities/example_fastq.fastq", (0,100), 1, 0)

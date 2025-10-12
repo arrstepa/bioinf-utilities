5f8f5627763ed4ef8e8cd1de730c2d0475127be3
arrstepa <arrstepa@gmail.com>
2025-10-12 22:10:21 +0300

    Add function convert_multiline_fasta_to_oneline

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,5 +1,29 @@
-def convert_multiline_fasta_to_oneline():
-    pass
+import os
 
-def parse_blast_output():
-    pass
+def convert_multiline_fasta_to_oneline(input_fasta):
+    """
+    A tool for converting multiline fasta files to oneline format
+
+    Arguments:
+    input_fastq: str
+
+    Returns str
+    """
+    oneline_fasta = []
+    current_seq = ''
+    with open(input_fasta) as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('>'):
+                if current_seq:
+                    oneline_fasta.append(current_seq)
+                    current_seq = ''
+                oneline_fasta.append(line)
+            else:
+                current_seq += line
+        if current_seq:
+            oneline_fasta.append(current_seq)
+    output_fasta = os.path.join(os.path.dirname(input_fasta), 'output_fasta.txt')
+    with open(output_fasta, 'a') as file:
+        for line in oneline_fasta:
+            file.write(line + "\n")
